9be853c84c4b82ec14010fa89ced548289b50678
Víctor <victor.alex911@gmail.com>
2013-02-18 22:31:52 -0600

    Detección de objetos

--- filtro.py
+++ filtro.py
@@ -289,27 +289,6 @@ def quitar_sal_pim(negros):
                     iter+=1
                     im.putpixel((i,j), (promedior,promediog,promediob))
 
-def dfs(pixel):
-    k = 0
-    (w,h)=im.size
-    lista = []
-    lista.append(pixel)
-    (r,g,b)=im.getpixel(pixel)
-    color = r #Suponiendo que la imagen esta en blanco y negro.
-    for x,y in lista:
-        for i in range(x-1, x+2):
-            for j in range(y-1, y+2):
-                if i >= 0 and j >= 0 and i < w and j < h:
-                    if im.getpixel((i,j))==(r,g,b):
-                        print r,g,b
-                        lista.append((i,j))
-                        im.putpixel((i,j), (255,0,0))                        
-                        print k
-                        im.save(str(k)+".png")
-                        k+=1
-                
-
-
 def bfs(pixel, imagen, pintura):
     k=0
     (w,h)=imagen.size
